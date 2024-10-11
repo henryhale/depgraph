43d17a0295ce70f4ade292267a921ac5f8e90645
Henry Hale <92443116+henryhale@users.noreply.github.com>
2024-10-11 20:38:43 +0300

    refactor(fix): produce unique and sane iddentifiers

--- export/mermaid.go
+++ export/mermaid.go
@@ -1,6 +1,7 @@
 package export
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/henryhale/depgraph/lang"
@@ -22,8 +23,7 @@ func Mermaid(deps *lang.DependencyGraph) string {
 	}
 
 	for file, analysis := range *deps {
-		fileId := strings.ReplaceAll(file, "/", "_")
-		fileId = strings.ReplaceAll(file, "@", "_")
+		fileId := cleanNodeId(file)
 
 		if idExists(fileId) {
 			continue
@@ -34,14 +34,14 @@ func Mermaid(deps *lang.DependencyGraph) string {
 
 		// exports as nodes in subgraph
 		for _, export := range analysis.Exports {
-			id := fileId + "_" + export
+			id := fileId + "_" + cleanNodeId(export)
 
-			if idExists(id) {
+			if idExists(file + id) {
 				continue
 			}
-			ids[id] = struct{}{}
+			ids[file + id] = struct{}{}
 
-			text := "[" + export + "]"
+			text := `["` + cleanLabel(export) + `"]`
 			subgraph += indent2 + id + text + nl
 		}
 
@@ -51,18 +51,17 @@ func Mermaid(deps *lang.DependencyGraph) string {
 
 		// resolve edges
 		for importedFile, items := range analysis.Imports {
-			iFileId := strings.ReplaceAll(importedFile, "/", "_")
-			iFileId = strings.ReplaceAll(importedFile, "@", "_")
+			iFileId := cleanNodeId(importedFile)
 			for _, item := range items {
 				itemnode := iFileId
 				if !strings.Contains(item, "*") {
 					itemnode += "_" + item
 				}
 
-				if idExists("edge" + itemnode) {
+				if idExists("$$edge$$" + file + itemnode) {
 					continue
 				}
-				ids["edge" + itemnode] = struct{}{}
+				ids["$$edge$$" + file + itemnode] = struct{}{}
 
 				edges += indent1 + fileId + "-->|uses|" + itemnode + nl
 			}
@@ -70,4 +69,14 @@ func Mermaid(deps *lang.DependencyGraph) string {
 	}
 
 	return graph + edges
+}
+
+func cleanNodeId(s string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9\/\._]`)
+	return re.ReplaceAllString(s, "_")
+}
+
+func cleanLabel(s string) string {
+	t := strings.ReplaceAll(s, "*", "#42;")
+	return t
 }
