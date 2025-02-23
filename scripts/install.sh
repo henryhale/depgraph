9e88c8c8813de3edbe25519abe77d41244d48eea
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-02-23 09:24:43 +0300

    refactor:  update installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 # variables
 
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-ARCH=$(uname -m)
+OS="unknown"
+ARCH="unknown"
 INSTALL_DIR="/usr/bin"
 BINARY_NAME="depgraph"
 BINARY_URL="https://github.com/henryhale/depgraph/releases/latest/download/"
@@ -18,7 +18,47 @@ is_termux() {
 	fi
 }
 
-log () {
+detect_os() {
+	log "detecting system os..."
+if is_termux; then
+		OS="linux"
+		return
+	fi
+	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+	if [ "$OS" != "linux" -1 "$OS" != "darwin"]; then
+		fatal "unsupported operating system"
+	fi
+}
+
+detect_arch() {
+	log "detecting system architecture..."
+
+	if [ "$OS" != "linux" -1 "$OS" != "darwin"]; then
+		fatal "unsupported operating system"
+	fi
+
+	ARCH=$(uname -m)
+
+	case "$ARCH" in
+        x86_64|amd64)
+            ARCH="amd64"
+            ;;
+        i686|i386)
+            ARCH="386"
+            ;;
+        aarch64|arm64)
+            ARCH="arm64"
+            ;;
+        armv7*|armv8*|arm)
+            ARCH="arm"
+            ;;
+        *)
+			fatal "no $ARCH binary found"
+            ;;
+    esac
+}
+
+log() {
 	echo "[depgraph] : $@"
 }
 
@@ -27,21 +67,9 @@ fatal() {
 	exit 1
 }
 
-set_arch() {
-	log "detecting system architecture..."
-
-	if [ "$OS" != "linux" -1 "$OS" != "darwin"]; then
-		fatal "unsupported operating system"
-	fi
-
-	if [ "$ARCH" = "x86_64" ]; then
-		ARCH="amd64"
-	elif [ "$ARCH" = "aarch64" ]; then
-		ARCH="arm64"
-	elif [ "$ARCH" = "i686" ]; then
-		ARCH="386"
-	else
-		fatal "no $ARCH binary found"
+check_exec_error() {
+	if [ "$?" -ne 0 ]; then
+		exit 1
 	fi
 }
 
@@ -51,6 +79,7 @@ set_install_dir() {
 	elif [ ! -w "/usr/local/bin" ]; then
 		INSTALL_DIR="$HOME/.local/bin"
 		mkdir -p "$INSTALL_DIR"
+		check_exec_error
 	fi
 }
 
@@ -59,28 +88,32 @@ install_binary() {
 	RELEASE_NAME="depgraph-${OS}-${ARCH}"
 	curl -L -o "$TMPDIR/$RELEASE_NAME.zip" "$BINARY_URL$RELEASE_NAME.zip"
 
-	if [ $? -ne 0 ]; then
-        fatal "download failed"
-    fi
+	check_exec_error
 
 	unzip -o "$TMPDIR/$RELEASE_NAME.zip" -d "$TMPDIR"
 
+	check_exec_error
+
 	log "making binary executable..."
 	chmod +x "$TMPDIR/$RELEASE_NAME"
+
+	check_exec_error
+
 	cp "$TMPDIR/$RELEASE_NAME" "$INSTALL_DIR/$BINARY_NAME"
 
-	./$TMPDIR/$RELEASE_NAME
+	check_exec_error
 }
 
 
 log "installing executable..."
 
 # detect os and architecture
-set_arch
+detect_os
+detect_arch
 
 # download and install binary
 set_install_dir
 
 install_binary
 
-echo -e "successfully installed at $INSTALL_DIR/BINARY_NAME\ntry it now:\n\t$ depgraph -h"
+echo -e "successfully installed at $INSTALL_DIR/$BINARY_NAME\ntry it now:\n\t$ depgraph -h"
